ececa48f54715bc33714353838576fa9894539ad
zdflower <zdflower@users.noreply.github.com>
2017-04-25 16:19:37 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,15 +15,16 @@ $(document).ready( function(){
 	var latitud;
 	var unidad = "metric";
 	var idioma = "es";
+	var c1 = "38fca250282f6fb95f6cbc49d295ea26";
 
 	//Obtener longitud y latitud
-	$.getJSON('https://crossorigin.me/http://ip-api.com/json/?callback=?', function(data){
+	$.getJSON('https://cors-anywhere.herokuapp.com/http://ip-api.com/json/?callback=?', function(data){
 		//console.log(data);
 		longitud = data["lon"];
 		latitud = data["lat"];
 
 		//Weather API
-		var api = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud + "&units="+ unidad + "&lang="+ idioma + "&APPID=" + c1;
+		var api = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud + "&units="+ unidad + "&lang="+ idioma + "&APPID=" + c1;
 		//var api = "http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud +"&APPID=" + clave;
 		$.getJSON(api, function(data){//este ejemplo tiene la temperatura en Celsius (metric)
 			console.log(data);
@@ -36,7 +37,7 @@ $(document).ready( function(){
 			$("#ciudad").html("<p>"+ciudad+"</p>");
 			$("#temperatura").html("<p>"+temperaturaC+"</p>");
 			$("#viento").html("<p>Velocidad del viento: " + viento + " m/s </p>");
-			$("#cielo").html("<p>" + cielo + " <img src='https://crossorigin.me/http://openweathermap.org/img/w/" + icono +".png'>" + " </p>");
+			$("#cielo").html("<p>" + cielo + " <img src='https://cors-anywhere.herokuapp.com/http://openweathermap.org/img/w/" + icono +".png'>" + " </p>");
 
 
 			/////////////PARTE A MEJORAR//////////////
