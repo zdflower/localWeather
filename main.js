bfd0540ca23319404f3990e6f032eae4a68265d3
zdflower <zdflower@users.noreply.github.com>
2017-04-25 16:32:35 -0300

    probando cosas, uso una ciudad fija

--- main.js
+++ main.js
@@ -11,52 +11,43 @@ function convertirC2F(celsius){
 }
 
 $(document).ready( function(){
-	var longitud;
-	var latitud;
+	var longitud = 145.77;
+	var latitud = -16.92;
 	var unidad = "metric";
 	var idioma = "es";
 	var c1 = "38fca250282f6fb95f6cbc49d295ea26";
-
-	//Obtener longitud y latitud
-	$.getJSON('https://cors-anywhere.herokuapp.com/http://ip-api.com/json/?callback=?', function(data){
-		//console.log(data);
-		longitud = data["lon"];
-		latitud = data["lat"];
-
-		//Weather API
-		var api = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud + "&units="+ unidad + "&lang="+ idioma + "&APPID=" + c1;
-		//var api = "http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud +"&APPID=" + clave;
-		$.getJSON(api, function(data){//este ejemplo tiene la temperatura en Celsius (metric)
-			console.log(data);
-			var temperaturaC = data["main"]["temp"];
-			var temperaturaF = convertirC2F(temperaturaC);
-			var cielo = data["weather"][0]["description"];
-			var viento = data["wind"]["speed"];
-			var ciudad = data["name"];
-			var icono = data["weather"][0]["icon"];
-			$("#ciudad").html("<p>"+ciudad+"</p>");
-			$("#temperatura").html("<p>"+temperaturaC+"</p>");
-			$("#viento").html("<p>Velocidad del viento: " + viento + " m/s </p>");
-			$("#cielo").html("<p>" + cielo + " <img src='https://cors-anywhere.herokuapp.com/http://openweathermap.org/img/w/" + icono +".png'>" + " </p>");
-
+	//Weather API
+	var api = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud + "&units="+ unidad + "&lang="+ idioma + "&APPID=" + c1;
+	//var api = "http://api.openweathermap.org/data/2.5/weather?lat="+ latitud +"&lon="+ longitud +"&APPID=" + clave;
+	$.getJSON(api, function(data){//este ejemplo tiene la temperatura en Celsius (metric)
+		console.log(data);
+		var temperaturaC = data["main"]["temp"];
+		var temperaturaF = convertirC2F(temperaturaC);
+		var cielo = data["weather"][0]["description"];
+		var viento = data["wind"]["speed"];
+		var ciudad = data["name"];
+		var icono = data["weather"][0]["icon"];
+		$("#ciudad").html("<p>"+ciudad+"</p>");
+		$("#temperatura").html("<p>"+temperaturaC+"</p>");
+		$("#viento").html("<p>Velocidad del viento: " + viento + " m/s </p>");
+		$("#cielo").html("<p>" + cielo + " <img src='https://cors-anywhere.herokuapp.com/http://openweathermap.org/img/w/" + icono +".png'>" + " </p>");
 
 			/////////////PARTE A MEJORAR//////////////
 			//Falta que los well tengan todos la misma altura y que la temperatura y la unidad estén en la misma línea
 			////////////////////////////////////////
 
 			//evento botón grados//
-			$("#CelsiusFa").on("click", function(){
-				if ($("#CelsiusFa").attr("value") == "c"){
-					$("#CelsiusFa").attr("value", "f");
-					$("#CelsiusFa").html("ºF");
-					$("#temperatura").html("<p>"+temperaturaF+"</p>");
-				} else{
-					//pasar de farenheit a celsius
-					$("#CelsiusFa").attr("value", "c");
-					$("#CelsiusFa").html("ºC");
-					$("#temperatura").html("<p>"+temperaturaC+"</p>");
-				}
-			});
+		$("#CelsiusFa").on("click", function(){
+			if ($("#CelsiusFa").attr("value") == "c"){
+				$("#CelsiusFa").attr("value", "f");
+				$("#CelsiusFa").html("ºF");
+				$("#temperatura").html("<p>"+temperaturaF+"</p>");
+			} else{
+				//pasar de farenheit a celsius
+				$("#CelsiusFa").attr("value", "c");
+				$("#CelsiusFa").html("ºC");
+				$("#temperatura").html("<p>"+temperaturaC+"</p>");
+			}
 		});
 	});
 });
